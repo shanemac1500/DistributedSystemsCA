19da36c702ea9c6c7666362c2e8694049a81ce59
shanemac1500 <C22737791@mytudublin.ie>
2025-10-22 17:28:14 +0100

    added FitnessService, DAO, model, and web.xml update

--- eclipse-workspace/DistributedSystemsCA/WEB-INF/src/com/fitnesscentre/rest/FitnessService.java
+++ eclipse-workspace/DistributedSystemsCA/WEB-INF/src/com/fitnesscentre/rest/FitnessService.java
@@ -1,8 +1,15 @@
 package com.fitnesscentre.rest;
 
+import java.util.List;
+
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+
+import com.fitnesscentre.DAO.MembershipPlanDAO;
+import com.fitnesscentre.model.MembershipPlan;
 
 @Path("/fitness")
 public class FitnessService {
@@ -14,5 +21,24 @@ public class FitnessService {
 		return "Hello from FitnessService!";
 	}
 	
+	//Get all Membership Plans
+	@GET
+	@Path("/json/plans")
+	@Produces("application/json")
+	public List<MembershipPlan> getAllPlans(){
+		MembershipPlanDAO dao = new MembershipPlanDAO();
+		return dao.findAll();
+	}
+	
+	//POST a new plan
+	@POST
+	@Path("/plan")
+	@Consumes("application/json")
+	@Produces("text/plain")
+	public String addPlan(MembershipPlan plan) {
+		MembershipPlanDAO dao = new MembershipPlanDAO();
+		dao.persist(plan);
+		return "Plan added: " + plan.getDescription();
+	}
 
 }
